b23bd54e5c75fe3a44b71ccca6acee67104e037c
dave <zhudy@yahoo.com>
2020-01-19 16:51:05 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by mac mini on 2020/1/19.
+//  Copyright © 2020 mac mini. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
