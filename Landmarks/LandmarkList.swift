eba9279bcf18acdbe155567db88f9b5383072323
dave <zhudy@yahoo.com>
2020-01-20 05:34:34 +0800

    section4-5

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by mac mini on 2020/1/20.
+//  Copyright © 2020 mac mini. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarkData) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
