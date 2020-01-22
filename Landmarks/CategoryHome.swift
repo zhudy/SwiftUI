1f03f5bda8a970b82238bd7795ee1bbbfce04d72
dave <zhudy@yahoo.com>
2020-01-22 17:15:26 +0800

    Add Navigation Between Sections 2do: https://developer.apple.com/tutorials/swiftui/working-with-ui-controls

--- Landmarks/CategoryHome.swift
+++ Landmarks/CategoryHome.swift
@@ -19,6 +19,17 @@ struct CategoryHome: View {
     var featured: [Landmark] {
         landmarkData.filter { $0.isFeatured }
     }
+    @State var showingProfile = false
+    
+    
+    var profileButton: some View {
+        Button(action: { self.showingProfile.toggle() }) {
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .accessibility(label: Text("User Profile"))
+                .padding()
+        }
+    }
     
     var body: some View {
         NavigationView {
@@ -30,8 +41,17 @@ struct CategoryHome: View {
                 ForEach(categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: self.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
+                
+                NavigationLink(destination: LandmarkList()) {
+                    Text("See All")
+                }
             }
             .navigationBarTitle(Text("Featured"))
+            .navigationBarItems(trailing: profileButton)
+            .sheet(isPresented: $showingProfile) {
+                Text("User Profile")
+            }
         }
     }
 }
--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -13,7 +13,6 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
 
     var body: some View {
-        NavigationView {
             List {
                 Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
@@ -28,17 +27,19 @@ struct LandmarkList: View {
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
-        }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-            .environmentObject(UserData())
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
+            NavigationView {
+
+                LandmarkList()
+                .environmentObject(UserData())
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+            }
         }
     }
 }
