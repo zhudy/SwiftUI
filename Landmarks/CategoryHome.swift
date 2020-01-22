3041f47c745c4f1723f3e4bd9862495236227ce7
dave <zhudy@yahoo.com>
2020-01-22 16:55:08 +0800

    section4: Compose the Home View

--- Landmarks/CategoryHome.swift
+++ Landmarks/CategoryHome.swift
@@ -10,21 +10,36 @@ import SwiftUI
 
 struct CategoryHome: View {
     var categories: [String: [Landmark]] {
-           Dictionary(
-               grouping: landmarkData,
-               by: { $0.category.rawValue }
-           )
-       }
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    
+    var featured: [Landmark] {
+        landmarkData.filter { $0.isFeatured }
+    }
     
     var body: some View {
         NavigationView {
             List {
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
                 ForEach(categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: self.categories[key]!)
                 }
             }
-            .navigationBarTitle("Featured")
+            .navigationBarTitle(Text("Featured"))
         }
+    }
+}
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image.resizable()
     }
 }
 
