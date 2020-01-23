da4e71e53db2c7ebda83ae13fd1c0a2d53550218
dave <zhudy@yahoo.com>
2020-01-23 20:04:35 +0800

    section2: Add an Edit Mode

--- Landmarks/Models/UserData.swift
+++ Landmarks/Models/UserData.swift
@@ -12,5 +12,6 @@ import Combine
 final class UserData: ObservableObject {
     @Published var showFavoritesOnly = false
     @Published var landmarks = landmarkData
+    @Published var profile = Profile.default
 }
 
