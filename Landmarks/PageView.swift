e2814245127a220b3896f195b0b1690496ccc5ac
dave <zhudy@yahoo.com>
2020-01-24 17:19:49 +0800

    refactor: move some swift to Support Views group

---
+++ Landmarks/PageView.swift
@@ -0,0 +1,21 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by mac mini on 2020/1/24.
+//  Copyright © 2020 mac mini. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView()
+    }
+}
---
+++ Landmarks/PageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by mac mini on 2020/1/24.
+//  Copyright © 2020 mac mini. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        PageViewController()
+    }
+}
