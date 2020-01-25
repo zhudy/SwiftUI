07c2dd03360d07f69afc6cf0555d2e19c9c2d2de
dave <zhudy@yahoo.com>
2020-01-25 10:45:18 +0800

    WebView branch works

---
+++ Landmarks/WebView.swift
@@ -0,0 +1,32 @@
+//
+//  WebView.swift
+//  Landmarks
+//
+//  Created by mac mini on 2020/1/25.
+//  Copyright © 2020 mac mini. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView : UIViewRepresentable {
+      
+    let request: URLRequest
+      
+    func makeUIView(context: Context) -> WKWebView  {
+        return WKWebView()
+    }
+      
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+      
+}
+  
+#if DEBUG
+struct WebView_Previews : PreviewProvider {
+    static var previews: some View {
+        WebView(request: URLRequest(url: URL(string: "https://www.apple.com")!))
+    }
+}
+#endif
